7a9cb411104055890bffce9a484428822d86c3ac
abishr <adhambishr@gmail.com>
2018-03-28 16:59:16 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+
+const destination = document.querySelector('#root')
+
+ReactDOM.render(<div><App/></div>, destination)
